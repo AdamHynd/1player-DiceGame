142c08b57175a58350d5cdef2c8b5db9e8ddba08
AdamHynd <105781671+AdamHynd@users.noreply.github.com>
2022-06-26 12:34:28 +0100

    Add files via upload
    My dice game which is unfinished as i cant get the dice to roll when the button is pressed

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const rollDice = document.querySelector("#RollBttn")
+const rollDice = document.querySelector("#rollBttn")
 const change = document.querySelector("#switchBttn")
 const message = document.querySelector("#message")
 const score = document.querySelector("#score")
 
-const winMsg = "Congrats! you win! rolld the dice to play again!"
-const loseMsg = "You lost! Better luck next time!"
+const winMsg = "Congrats! you win! \nroll the dice to play again!"
+const loseMsg = "You lost! \nBetter luck next time!"
 
 let gamescore = 0;
 let fail = 1;
 let win =20;
+let gameover = true;
 // A variable with all of the images inside 
 let diceside = {
+    d0:"./Images/0.png",
     d1:"./Images/1.png",
     d2:"./Images/2.png",
     d3:"./Images/3.png",
@@ -21,34 +23,43 @@ let diceside = {
 
 // I think this might be where ive gone wrong as it dosent roll when i press the button 
 const rollDie = ()=>{
-    randie = Math.floor((math.random()*6)+1)
+    ranDie = Math.floor((math.random()*6)+1)
     document.getElementById("dice").src = diceside["d" + ranDie];
-    score = (score + randie);
+    gamescore = (gamescore + ranDie);
     currentscore.textContent = gamescore;
 
 // this is for when you hit a 1 on the dice and its an instant loose 
-    if (randie == fail && gamescore <= winValue){
-        gamescore =0;
-        currentscore.textContent= 0;
+    if (ranDie == fail && gamescore <= winValue){
+        gameover = true       
         themessage.innerText = loseMsg
         document.getElementById("message").style.backgroundColor = "red"
-        document.getElementById(message).style.color = "white"
+        document.getElementById("message").style.color = "white"
     }
     // for when the number 20 is reached and you win 
     else if (gamescore >= winvalue) {
-        gamescore = 0;
-        currentscore.textContent = 0;
+        gameover = true
         themessage.innerText = winMsg
         document.getElementById("message").style.backgroundColor = "green"
         document.getElementById("message").style.color = "white"
     }
     // for when you havent rolled a 1 yet and havent reached the number 20 
     else {
-        themessage.textContent = "Keep Rolling"
+        themessage.innerText = "Keep Rolling"
         document.getElementById("message").style.backgroundColor = "wheat"
         document.getElementById("message").style.color = "blueviolet"
     }
-
+const checkGameStatus = ()=>{
+    if (gameover == true) {
+        gamescore= 0;
+        currentscore.textContent = 0;
+        gameover = false
+        rollDice()
+    }
+    else {
+        rollDice()
+    }
+    
+}
 }
 
-rollDice.addEventListener("click", rollDie)
+rollDice.addEventListener("click", checkGameStatus)
